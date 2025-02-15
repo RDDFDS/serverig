544f048395522c8a1967afb1dfd6c7631e599e8c
Flammy <86204487+RDDFDS@users.noreply.github.com>
2025-02-15 21:43:10 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+import eventlet
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 socketio = SocketIO(app)
