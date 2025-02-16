7a97b75bc38709412214612c96887ded3f76b6a6
Flammy <86204487+RDDFDS@users.noreply.github.com>
2025-02-16 09:34:18 +0000

    Update server.py

--- server.py
+++ server.py
@@ -5,23 +5,42 @@ app = Flask(__name__)
 
 def get_system_info():
     return {
-        "Sistema Operativo": platform.system(),
-        "Versão do SO": platform.version(),
-        "Arquitetura": platform.architecture()[0],
-        "Processador": platform.processor(),
+        "Server machine name": platform.node(),
+        "OS": platform.system(),
+        "OS version": platform.version(),
+        "Arq.": platform.architecture()[0],
+        "Processor": "Intel® Core™ i9 14900KS -- 6.20 GHz -- Limited by Host"
         "CPU Cores": psutil.cpu_count(logical=False),
         "Threads": psutil.cpu_count(logical=True),
-        "Memória Total": f"{round(psutil.virtual_memory().total / (1024 ** 3), 2)} GB",
-        "Uso de CPU": f"{psutil.cpu_percent()}%",
-        "Uso de RAM": f"{psutil.virtual_memory().percent}%",
-        "IP do Servidor": socket.gethostbyname(socket.gethostname())
+        "Total Mem.": f"{round(psutil.virtual_memory().total / (1024 ** 3), 2)} GB",
+        "CPU Usage": f"{psutil.cpu_percent()}%",
+        "RAM Usage": f"{psutil.virtual_memory().percent}%",
+        "Server IP": socket.gethostbyname(socket.gethostname())
+    }
+
+def get_client_info(request):
+    return {
+        "IP": request.remote_addr,
+        "Machine": socket.getfqdn(request.remote_addr),
+        "OS": platform.system(),
+        "SO version": platform.version(),
+        "Arq.": platform.architecture()[0],
+        "Processor": platform.processor(),
+        "CPU Cores": psutil.cpu_count(logical=False),
+        "Threads": psutil.cpu_count(logical=True),
+        "Total Mem.": f"{round(psutil.virtual_memory().total / (1024 ** 3), 2)} GB",
+        "CPU Usage": f"{psutil.cpu_percent()}%",
+        "RAM Usage": f"{psutil.virtual_memory().percent}%",
+        "User-Agent": request.headers.get('User-Agent'),
+        "Accept Lang.": request.headers.get('Accept-Language'),
+        "Accept Encoding": request.headers.get('Accept-Encoding')
     }
 
 @app.route('/')
 def index():
-    client_ip = request.remote_addr
+    client_info = get_client_info(request)
     server_info = get_system_info()
-    return render_template('index.html', client_ip=client_ip, server_info=server_info)
+    return render_template('index.html', client_info=client_info, server_info=server_info)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
